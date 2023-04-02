699e10e1285e710f67cadcc9d409e370d25ed8dc
YaroslavPr17 <pristalovya@yandex.ru>
2023-04-02 18:58:20 +0300

    chore: Training Catboost & LightGBM model via GPU

--- src/nlp/embeddings.py
+++ src/nlp/embeddings.py
@@ -1,5 +1,6 @@
 import sys
 from typing import List
+import os
 
 import pandas as pd
 
@@ -59,11 +60,12 @@ def corp2vecs(corpus: pd.Series,
         if model_type == 'word2vec':
             model = Word2Vec(sentences=tokenized_corpus, vector_size=emb_size, workers=n_treads, epochs=n_epochs)
         elif model_type == 'fasttext':
-            training_filepath = '_train.tmp'
+            training_filepath = 'tmp_files/_train_all_reviews.tmp'
             with open(training_filepath, 'wt', encoding='utf-8') as file:
                 file.write('         '.join(corpus))
             model = fasttext.train_unsupervised(training_filepath, model='skipgram', dim=emb_size, thread=n_treads,
                                                 epoch=n_epochs)
+            os.remove(training_filepath)
         else:
             raise NotImplementedError
     else:
