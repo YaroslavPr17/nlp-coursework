2ed1760c8887c41b38b104133d629b777a2694f4
YaroslavPr17 <pristalovya@yandex.ru>
2023-04-27 11:11:05 +0300

    refactor: Rename datasets folder & logreg files

--- datasets_.py
+++ datasets_.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-from typing import Literal
+from typing import Literal, Tuple
 
 import pandas as pd
+from sklearn.model_selection import train_test_split as train_test_split_
 
 from src.kinopoisk_analyzer.utils.constants import datasets_path
 
@@ -12,7 +13,12 @@ class DatasetLoader:
     def load_reviews_Review_Label_dataset(cls,
                                           tokenizer: Literal['razdel', 'rutokenizer'] = None,
                                           stopwords: Literal['no', 'nltk'] = 'nltk',
-                                          show_path: bool = False) -> pd.DataFrame:
+                                          train_test_split: bool = False,
+                                          test_size: float = 0.3,
+                                          remove_neutral_class: bool = False,
+                                          classnames_to_int: bool = False,
+                                          random_state=42,
+                                          show_path: bool = False) -> pd.DataFrame | Tuple[pd.DataFrame, pd.DataFrame]:
         subfolder = 'reviews_Review_Label'
         if tokenizer:
             filename = f'{subfolder}_{tokenizer}_{stopwords}.csv'
@@ -24,6 +30,25 @@ class DatasetLoader:
 
         try:
             dataset = pd.read_csv(Path(datasets_path, subfolder, filename), index_col=0)
+
+            if classnames_to_int:
+                label_encoding = {
+                    'POSITIVE': 2,
+                    'NEUTRAL': 1,
+                    'NEGATIVE': 0
+                }
+                dataset.label = dataset.label.apply(lambda label: label_encoding[label])
+
+            if remove_neutral_class:
+                if classnames_to_int:
+                    neutral_repr = 1
+                else:
+                    neutral_repr = 'NEUTRAL'
+                dataset = dataset[dataset.label != neutral_repr].reset_index().drop(columns=['index'])
+
+            if train_test_split:
+                train, test = train_test_split_(dataset, test_size=test_size, random_state=random_state)
+                return train, test
             return dataset
         except FileNotFoundError:
             print(f"No file named '{filename}' in dataset directory.")
