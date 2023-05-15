30978386791a4a3d08b479c4edfcf0f1b9cce21b
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2023-05-15 22:44:30 +0300

    refactor: Transferred constant notebook code into .py

--- src/nlp/application.py
+++ src/nlp/application.py
@@ -9,7 +9,236 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 
 from nltk.tokenize import WhitespaceTokenizer
+
+import torch
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from torch import nn
+from torch.utils.data import Dataset, DataLoader
 from tqdm.notebook import tqdm
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+
+
+class ReviewDataset(Dataset):
+    def __init__(self, reviews, labels, tokenizer, max_model_input_length=512):
+        self.reviews = reviews
+        self.labels = labels
+        self.tokenizer = tokenizer
+        self.max_model_input_length = max_model_input_length
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        review = self.reviews.iloc[idx]
+        label = self.labels.iloc[idx]
+        review_tokenized = self.tokenizer(
+            review,
+            add_special_tokens=True,
+            max_length=self.max_model_input_length,
+            padding='max_length',
+            return_attention_mask=True,
+            return_tensors='pt',
+            truncation=True,
+        )
+        input_ids = review_tokenized['input_ids'].flatten()
+        attn_mask = review_tokenized['attention_mask'].flatten()
+
+        return {
+            'review': review,
+            'input_ids': input_ids,
+            'attention_mask': attn_mask,
+            'label': label,
+        }
+
+
+class BertClassifier:
+    def __init__(self, checkpoint, n_classes=2):
+
+        self.model = AutoModelForSequenceClassification.from_pretrained(checkpoint, local_files_only=False,
+                                                                        ignore_mismatched_sizes=True)
+        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.max_len = self.model.config.max_position_embeddings
+        self.out_features = self.model.config.pooler_fc_size
+        self.model.classifier = torch.nn.Linear(self.out_features, n_classes)
+        self.model.to(self.device)
+        self.loss_fn = nn.CrossEntropyLoss()
+
+        self.optimizer = torch.optim.Adam(self.model.classifier.parameters(), lr=1e-3)
+        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer, step_size=1, gamma=0.5)
+
+        self.all_losses = []
+        self.epoch_losses = []
+        self.epoch_acc = []
+
+    def fit(self, train_dataloader: torch.utils.data.DataLoader):
+        self.model.train()
+        losses = []
+        correct_predictions = 0
+
+        t = tqdm(train_dataloader, file=sys.stdout, ncols=100)
+
+        for data in t:
+            input_ids = data['input_ids'].to(self.device)
+            attention_mask = data['attention_mask'].to(self.device).to(float)
+            labels = data['label'].to(self.device)
+
+            outputs = self.model(
+                input_ids=input_ids,
+                attention_mask=attention_mask
+            )
+
+            preds = outputs.logits.argmax(dim=1)
+
+            loss = self.loss_fn(outputs.logits, labels)
+
+            correct_predictions += torch.sum(preds == labels)
+
+            losses.append(loss.item())
+
+            loss.backward()
+            self.optimizer.step()
+            self.optimizer.zero_grad()
+
+            t.set_postfix(ordered_dict={'loss': loss.item()}, refresh=True)
+
+        train_acc = correct_predictions.double() / len(train_dataloader.dataset)
+        train_loss = np.mean(losses)
+        self.all_losses.extend(losses)
+        self.epoch_losses.append(train_loss)
+        self.epoch_acc.append(train_acc)
+        return train_acc, train_loss
+
+    def evaluate(self, test_dataloader: DataLoader):
+        self.model.eval()
+        losses = []
+        correct_predictions = 0
+
+        all_preds = []
+        all_labels = []
+
+        t = tqdm(test_dataloader, file=sys.stdout, ncols=100)
+
+        with torch.no_grad():
+            for data in t:
+                input_ids = data["input_ids"].to(self.device)
+                attention_mask = data["attention_mask"].to(self.device)
+                labels = data["label"].to(self.device)
+
+                outputs = self.model(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask
+                )
+
+                preds = torch.argmax(outputs.logits, dim=1)
+                loss = self.loss_fn(outputs.logits, labels)
+                correct_predictions += torch.sum(preds == labels)
+
+                all_preds.extend(preds.tolist())
+                all_labels.extend(labels)
+
+                losses.append(loss.item())
+
+                t.set_postfix(ordered_dict={'loss': loss.item()}, refresh=True)
+
+        print('Classification report:')
+        print(classification_report(test_dataloader.dataset.labels, all_preds))
+
+        val_acc = correct_predictions.double() / len(test_dataloader.dataset)
+        val_loss = np.mean(losses)
+        return val_acc.item(), val_loss
+
+    def train(self, n_epochs, pretrain_test=False):
+        try:
+            if pretrain_test:
+                print('Pre-training test:')
+                val_acc, val_loss = self.evaluate()
+                print(f'Test loss {val_loss} accuracy {val_acc}')
+                print('-' * 10)
+
+            for epoch in range(n_epochs):
+                print(f'Epoch {epoch + 1}/{n_epochs}')
+                train_acc, train_loss = self.fit()
+                print(f'Train loss {train_loss} accuracy {train_acc}')
+
+                val_acc, val_loss = self.evaluate()
+                print(f'Test loss {val_loss} accuracy {val_acc}')
+                print('-' * 10)
+
+                # self.scheduler.step()
+
+        except KeyboardInterrupt:
+            print('Training was manually stopped. ')
+
+
+class BertLogRegClassifier:
+    def __init__(self, checkpoint):
+
+        self.model = AutoModelForSequenceClassification.from_pretrained(checkpoint)
+        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+        self.max_len = self.model.config.max_position_embeddings
+        self.out_features = self.model.config.pooler_fc_size
+        self.model.dropout = torch.nn.Sequential()
+        self.model.classifier = torch.nn.Sequential()
+
+        self.classifier = LogisticRegression(max_iter=2000, n_jobs=-1, random_state=42)
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+
+        self.all_train_embeddings = None
+        self.all_test_embeddings = None
+
+    def fit(self, train_dataloader: torch.utils.data.DataLoader):
+        self.model.eval()
+
+        self.all_train_embeddings = np.array([])
+        all_preds = []
+
+        t = tqdm(train_dataloader, file=sys.stdout, ncols=100)
+
+        for data in t:
+            with torch.no_grad():
+                input_ids = data['input_ids'].to(self.device)
+                attention_mask = data['attention_mask'].to(self.device).to(float)
+                labels = data['label'].to(self.device)
+
+                embeddings = self.model(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask
+                ).logits
+
+                self.all_train_embeddings = np.append(self.all_train_embeddings, embeddings.cpu().numpy())
+                all_preds.extend(labels)
+
+        self.all_train_embeddings = self.all_train_embeddings.reshape(-1, self.out_features)
+
+        self.classifier.fit(self.all_train_embeddings, all_preds)
+
+    def predict(self, test_input):
+        self.model.eval()
+
+        self.all_test_embeddings = np.array([])
+
+        t = tqdm(test_input, file=sys.stdout, ncols=100)
+
+        for data in t:
+            with torch.no_grad():
+                input_ids = data['input_ids'].to(self.device)
+                attention_mask = data['attention_mask'].to(self.device).to(float)
+
+                embeddings = self.model(
+                    input_ids=input_ids,
+                    attention_mask=attention_mask
+                ).logits
+
+                self.all_test_embeddings = np.append(self.all_test_embeddings, embeddings.cpu().numpy())
+
+        self.all_test_embeddings = self.all_test_embeddings.reshape(-1, self.out_features)
+
+        return self.classifier.predict(self.all_test_embeddings)
 
 
 class Pipeline:
