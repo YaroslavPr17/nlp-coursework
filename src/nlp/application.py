2763a9c2610ac6d37fbd9f9863e3121088e95c8b
YaroslavPr17 <yarikpristalov1702@yandex.ru>
2023-05-18 17:47:43 +0300

    feat: Add consistent text cleaning facilities in old interface

--- src/nlp/application.py
+++ src/nlp/application.py
@@ -18,6 +18,8 @@ from torch.utils.data import Dataset, DataLoader
 from tqdm.notebook import tqdm
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
+from src.nlp.classification import Pipeline
+
 
 class ReviewDataset(Dataset):
     def __init__(self, reviews, labels, tokenizer, max_model_input_length=512):
@@ -251,77 +253,6 @@ class BertLogRegClassifier:
         return all_preds
 
 
-class Pipeline:
-    def __init__(self, task: str,
-                 model=None,
-                 preprocess=False,
-                 model_type: str = 'linear',
-
-                 ):
-        import pandarallel
-        pandarallel.pandarallel.initialize(nb_workers=os.cpu_count(), verbose=0)
-
-        _task_types = ['classification', 'summarization']
-
-        assert task in ['classification', 'summarization'], f'Wrong type of task. Expected: {_task_types}'
-
-        self.task = task
-        self.model = model
-        self.preprocess = preprocess
-        self.model_type = model_type
-        self.vectorizer = None
-        self.ret_value = None
-
-    def classify(self, docs: Iterable[str], return_confs=False):
-        NEGATIVE_THRESHOLD = 0.35
-
-        if self.preprocess:
-            # print('Preprocessing...')
-            from src.nlp.preprocessing import clean
-            if isinstance(docs, pd.Series) and os.cpu_count() > 1:
-                docs = docs.parallel_apply(lambda doc: clean(doc, tokenizer_type='razdel', stopwords=[]))
-            else:
-                docs = list(map(lambda doc: clean(doc, tokenizer_type='razdel', stopwords=[]),
-                                docs))
-
-        # print('Vectorization...')
-
-        if self.model_type == 'linear' and self.model is None:
-            with open('models/logreg_tokrazdel_stopno_100k.model', 'rb') as f:
-                self.model = dill.load(f)
-
-            with open('models/count_vectorizer_1_4_100000.vocab', 'rb') as f:
-                vocab = dill.load(f)
-                self.vectorizer = CountVectorizer(tokenizer=WhitespaceTokenizer().tokenize,
-                                                  vocabulary=vocab,
-                                                  ngram_range=(1, 4))
-
-        if self.model_type == 'lightgbm' and self.model is None:
-            with open('models/lightgbm_tokrazdel_stopno_100k.model', 'rb') as f:
-                self.model = dill.load(f)
-
-            with open('models/lightgbm_count_vectorizer_1_4_100000.vocab', 'rb') as f:
-                vocab = dill.load(f)
-                self.vectorizer = CountVectorizer(tokenizer=WhitespaceTokenizer().tokenize,
-                                                  vocabulary=vocab,
-                                                  ngram_range=(1, 4))
-
-        # print('Classification...')
-        X_texts = self.vectorizer.transform(docs)
-
-        if return_confs:
-            if self.model_type == 'linear':
-                self.ret_value = self.model.decision_function(X_texts)
-
-        # probas = self.model.predict_proba(X_texts.astype(np.float64))
-        # classes = np.where(probas > NEGATIVE_THRESHOLD, 0, 2)[:, 0]
-
-        return self.model.predict(X_texts.astype(np.float64)), self.ret_value
-
-    def __call__(self, docs: Iterable[str], return_confs=False, *args, **kwargs):
-        if self.task == 'classification':
-            return self.classify(docs=docs,
-                                 return_confs=return_confs)
 
 
 def get_df_by_person(data: pd.DataFrame, name: str) -> pd.DataFrame:
@@ -339,10 +270,14 @@ def collect_sents_to_summarize(data: pd.DataFrame, n_sents: int = 100) -> List[s
     all_sents = []
     for sents in data['occurrences']:
         all_sents.extend(sents)
+
+    if not len(all_sents):
+        print()
+        return all_sents
     all_sents = np.array(all_sents)
     pl = Pipeline('classification', model_type='linear', preprocess=True)
-    _, confs = pl(pd.Series(all_sents), return_confs=True)
-
+    _, dct = pl(pd.Series(all_sents), return_confs=True)
+    confs = dct['confs']
     to_summarize = []
     to_summarize.extend(all_sents[confs < 0].tolist())
     _n = n_sents - len(to_summarize)
--- src/nlp/preprocessing.py
+++ src/nlp/preprocessing.py
@@ -2,50 +2,53 @@ import re
 from typing import Callable, List
 
 
-def clean(review: str, tokenizer_type: str = None, stopwords: List[str] = None,
-          lemmatizer_type: str = 'pymorphy2', tagger=None, lemmatizer=None, char_clean_only: bool = False,
-          lowercase: bool = False):
-    _html_pattern = r'<[^<]+?>'
-    _escape_pattern = r'[\n|\r|\b]'
+def clean(review: str, lowercase: bool = False, char_clean: bool = False, lemmatize: bool = False,
+          tokenize: bool = False, remove_stopwords: bool = False,
+          tokenizer_type: str = None, lemmatizer_type: str = 'pymorphy2', stopwords: List[str] = None,
+          tagger=None, lemmatizer=None):
 
     if lowercase:
         review = review.lower()
 
-    review = re.sub(_html_pattern, '  ', review)
-    review = re.sub(_escape_pattern, '  ', review)
+    if char_clean:
+        _html_pattern = r'<[^<]+?>'
+        _escape_pattern = r'[\n|\r|\b]'
+        review = re.sub(_html_pattern, '  ', review)
+        review = re.sub(_escape_pattern, '  ', review)
 
-    if char_clean_only:
-        return review
+    if tokenize:
+        if tokenizer_type == 'razdel':
+            import razdel
+            review_tokens = [token.text for token in list(razdel.tokenize(review))]
 
-    if tokenizer_type == 'razdel':
-        import razdel
-        review_tokens = [token.text for token in list(razdel.tokenize(review))]
+        elif tokenizer_type == 'TreebankWordTokenizer':
+            from nltk.tokenize import TreebankWordTokenizer
+            review_tokens = TreebankWordTokenizer().tokenize(review)
 
-    elif tokenizer_type == 'TreebankWordTokenizer':
-        from nltk.tokenize import TreebankWordTokenizer
-        review_tokens = TreebankWordTokenizer().tokenize(review)
+        elif tokenizer_type == 'rutokenizer':
+            import rutokenizer
+            tokenizer = rutokenizer.Tokenizer()
+            tokenizer.load()
+            review_tokens = tokenizer.tokenize(review)
+        else:
+            raise NotImplementedError('Unknown tokenizer')
 
-    elif tokenizer_type == 'rutokenizer':
-        import rutokenizer
-        tokenizer = rutokenizer.Tokenizer()
-        tokenizer.load()
-        review_tokens = tokenizer.tokenize(review)
-    else:
-        raise NotImplementedError('Unknown tokenizer')
+        if lemmatize:
+            if lemmatizer_type == 'pymorphy2':
+                import pymorphy2
+                morph = pymorphy2.MorphAnalyzer(lang='ru')
+                review_tokens = [morph.parse(token)[0].normal_form for token in review_tokens]
 
-    if lemmatizer_type == 'pymorphy2':
-        import pymorphy2
-        morph = pymorphy2.MorphAnalyzer(lang='ru')
-        clear_review = [morph.parse(token)[0].normal_form for token in review_tokens]
+            elif lemmatizer_type == 'rulemma':
+                tags = tagger.tag(review_tokens)
+                lemmas = lemmatizer.lemmatize(tags)
+                review_tokens = list(map(lambda tpl: tpl[2], lemmas))
+            else:
+                raise NotImplementedError('Unknown lemmatizer')
 
-    elif lemmatizer_type == 'rulemma':
-        tags = tagger.tag(review_tokens)
-        lemmas = lemmatizer.lemmatize(tags)
-        clear_review = list(map(lambda tpl: tpl[2], lemmas))
-    else:
-        raise NotImplementedError('Unknown lemmatizer')
+        if remove_stopwords:
+            review_tokens = [token for token in review_tokens if token not in stopwords]
 
-    clean_review = [token for token in clear_review if token not in stopwords]
-    final_string = ' '.join(clean_review)
+        review = ' '.join(review_tokens)
 
-    return final_string
+    return review
